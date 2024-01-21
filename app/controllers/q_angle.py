fix: q angle calculation

app/controllers/q_angle.py
@@ -4,7 +4,11 @@ import cv2 as cv
 import mediapipe as mp
 import math
 import numpy as np
-from controllers._old.camera import Camera, Frame
+from utils.theme import Colors
+from utils.color_detection import ColorDetection, Point
+from utils.camera import Camera, Frame
+
+global_colors = Colors()
 
 class QAngle:
     def __init__(self):
@@ -14,28 +18,20 @@ class QAngle:
         self.mp_draw_style = mp.solutions.drawing_styles
         self.results = []
 
-    def find_distance(self, x1, y1, x2, y2):
-        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    
-    def find_endpoint(self, point, distance, angle):
-        rad = math.radians(angle)
+    def calc_angle(self, point1: Point, point2: Point):
+        x1, y1 = point1.x, point1.y
+        x2, y2 = point2.x, point2.y
         
-        x = int(point[0] + distance * np.sin(rad))
-        y = int(point[1] + distance * np.cos(rad))
-        
-        return x, y
-    
-    def find_angle(self, x1, y1, x2, y2):
         theta = math.acos( (y2 -y1)*(-y1) / (math.sqrt(
         (x2 - x1)**2 + (y2 - y1)**2 ) * y1) )
         degree = int(180/math.pi)*theta
 
         return degree
     
-    def find_angle_mid(self, p1, p2, p3):
-        x1, y1 = p1
-        x2, y2 = p2
-        x3, y3 = p3
+    def calc_angle_mid(self, p1: Point, p2: Point, p3: Point):
+        x1, y1 = p1.x, p1.y
+        x2, y2 = p2.x, p2.y
+        x3, y3 = p3.x, p3.y
         
         # Vektor BA dan BC
         vec_BA = (x1 - x2, y1 - y2)
@@ -50,103 +46,63 @@ class QAngle:
 
         # Cosinus theta
         cos_theta = dot_product / (mag_BA * mag_BC)
+        # Ensure the argument is within the valid range [-1, 1]
+        cos_theta = max(min(cos_theta, 1), -1)
 
         # Sudut dalam radian
         theta_rad = math.acos(cos_theta)
         degree = math.degrees(theta_rad)
 
         return degree 
+    
+    def calc_distance(self, p1: Point, p2: Point):
+        x1, y1 = p1.x, p1.y
+        x2, y2 = p2.x, p2.y
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
         
-    def get_landmarks(self, frame):
-        frame.flags.writeable = False
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        results = self.pose.process(frame)
-        frame.flags.writeable = True
-        frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
-
-        lm = results.pose_landmarks
-        lm_pose = self.mp_pose.PoseLandmark
-
-        return lm, lm_pose
-    
-    def get_keypoints(self, lm, lm_pose, w, h):
-        keypoints = {}
-        # Left hip
-        keypoints["l_hip_x"] = int(lm.landmark[lm_pose.LEFT_HIP].x * w)
-        keypoints["l_hip_y"] = int(lm.landmark[lm_pose.LEFT_HIP].y * h)
-        # Left knee
-        keypoints["l_knee_x"] = int(lm.landmark[lm_pose.LEFT_KNEE].x * w)
-        keypoints["l_knee_y"] = int(lm.landmark[lm_pose.LEFT_KNEE].y * h)
-        # Left ankle
-        keypoints["l_ankle_x"] = int(lm.landmark[lm_pose.LEFT_ANKLE].x * w)
-        keypoints["l_ankle_y"] = int(lm.landmark[lm_pose.LEFT_ANKLE].y * h)
+    def find_endpoint(self, point: Point, distance, angle, side=1):
         
-        return keypoints
+        if side == 1:
+            rad = math.radians(angle+180)
+            x = int(point.x - distance * np.sin(rad))
+            y = int(point.y - distance * np.cos(rad))
+        elif side == -1:
+            rad = math.radians(angle)
+            x = int(point.x + distance * np.tan(rad))
+            y = int(point.y + distance * np.cos(rad))
+        
+        return Point(x, y)
     
-    def show_landmarks(self, frame, lm):
-        self.mp_draw.draw_landmarks(
-            frame, 
-            lm, 
-            self.mp_pose.POSE_CONNECTIONS, 
-            landmark_drawing_spec=self.mp_draw_style.get_default_pose_landmarks_style()
-        )
+    def interpret(self, angle):
+        if (angle < 10):
+            return 'bow legged (genu valgum)'
+        elif (angle < 15):
+            return 'normal'
+        else:
+            return 'knok-kneed (genu varum)'
     
-    def draw_line_over(self, frame, center, radius, end_angle, direction, color = 'yellow'):
+    def draw_line_over(self, frame, center: Point, radius, end_angle, direction, color = global_colors.yellow):
         # Convert angles to radians
         end_angle_rad = math.radians(end_angle)
 
         # Calculate end points of the line
         if direction == 1:
             end_point = (
-                int(center[0] - radius * np.sin(end_angle_rad)),
-                int(center[1] - radius * np.cos(end_angle_rad))
+                int(center.x - radius * np.sin(end_angle_rad)),
+                int(center.y - radius * np.cos(end_angle_rad))
             )
         elif direction == -1:
             end_point = (
-                int(center[0] + radius * np.sin(end_angle_rad)),
-                int(center[1] + radius * np.cos(end_angle_rad))
+                int(center.x + radius * np.sin(end_angle_rad)),
+                int(center.y + radius * np.cos(end_angle_rad))
             )
         
-        cv.line(frame, center, end_point, color, 2)
+        cv.line(frame, (center.x, center.y), end_point, color, 2)
     
-    def draw_angle_indicator(self, frame, center, radius, start_angle, end_angle, color):
-        # Convert angles to radians
-        start_angle_rad = math.radians(start_angle)
-        end_angle_rad = math.radians(end_angle)
-
-        # Calculate start and end points of the arc
-        start_point = (
-            int(center[0] - radius * np.cos(start_angle_rad)),
-            int(center[1] - radius * np.sin(start_angle_rad))
-        )
-        end_point = (
-            int(center[0] - radius * np.cos(end_angle_rad)),
-            int(center[1] - radius * np.sin(end_angle_rad))
-        )
-
-        # Draw the arc on the frame
-        cv.ellipse(frame, center, (radius, radius), 0, start_angle, end_angle, color, 2)
-
-        # Draw lines connecting the center to the start and end points
-        cv.line(frame, center, start_point, color, 2)
-        cv.line(frame, center, end_point, color, 2)
-    
-    def get_angle(self, fr, frame, keypoints, w, h, font, colors):
-        l_hip_x = keypoints["l_hip_x"]
-        l_hip_y = keypoints["l_hip_y"]
-        l_knee_x = keypoints["l_knee_x"]
-        l_knee_y = keypoints["l_knee_y"]
-        l_ankle_x = keypoints["l_ankle_x"]
-        l_ankle_y = keypoints["l_ankle_y"]
-        
-        green = colors["green"]
-        red = colors["red"]
-        yellow = colors["yellow"]
-        pink = colors["pink"]
-        
-        
-
     def run(self):
+        colors = Colors()
+        detect = ColorDetection()
+        fr = Frame()
         camera = Camera()
         camera.is_opened()
         
@@ -156,14 +112,68 @@ class QAngle:
                 print("Error: Failed to capture frame.")
                 break
             
-            fr = Frame(frame)
-            frame = fr.frame
-            lm, lm_pose = self.get_landmarks(frame)
-            if lm:
-                keypoints = self.get_keypoints(lm, lm_pose, fr.width, fr.height)
-                self.get_angle(fr, frame, keypoints, fr.width, fr.height, fr.font, fr.colors)
+            contours = detect.get_objects(frame)
+            # Sort contours based on y-coordinate (top to bottom)
+            contours = sorted(contours, key=lambda c: cv.boundingRect(c)[1])
             
-            self.show_landmarks(frame, lm)
+            keypoints = []
+            
+            for i, contour in enumerate(contours, start=1):
+                # Get the bounding box of the contour
+                point_x, point_y, w, h = cv.boundingRect(contour)
+                
+                point = Point(point_x + w // 2, point_y + h // 2)
+
+                keypoints.append(point)
+
+                # Draw a rectangle around the detected object and label
+                cv.rectangle(frame, (point_x, point_y), (point_x+w, point_y+h), colors.green, 2)
+                fr.circle(frame, (point.x, point.y))
+                fr.put_text(frame, str(i), (point.x + 10, point.y), color=colors.white)
+            #endfor
+            
+            fr.meta_info(frame, 'Keypoints req: 3', 'bottom_left', (0, -100), fontSize=1.2)
+            fr.meta_info(frame, 'Keypoints count: ' + str(len(keypoints)), 'bottom_left', (0, -50), fontSize=1.2)
+            
+            # Connect keypoints with lines
+            if len(keypoints) == 3:
+                fr.meta_info(frame, 'Keypoints status: valid', 'bottom_left', fontSize=1.2, color=colors.green)
+                
+                # Draw line to each keypoints
+                for i in range(len(keypoints) - 1):
+                    fr.line(frame, (keypoints[i].x, keypoints[i].y), (keypoints[i + 1].x, keypoints[i + 1].y))
+                
+                asis = Point(keypoints[0].x, keypoints[0].y)
+                cPatella = Point(keypoints[1].x, keypoints[1].y)
+                tibialTub = Point(keypoints[2].x, keypoints[2].y)
+                
+                cPatella_tibialTub_angle = self.calc_angle(cPatella, tibialTub)
+                                
+                dist_cPatella_tibialTub = self.calc_distance(cPatella, tibialTub)
+                
+                # Find endpoint of the line
+                if tibialTub.x > cPatella.x:
+                    point_tibialTub_over = self.find_endpoint(cPatella, dist_cPatella_tibialTub, cPatella_tibialTub_angle, -1)
+                elif tibialTub.x < cPatella.x:
+                    point_tibialTub_over = self.find_endpoint(cPatella, dist_cPatella_tibialTub, cPatella_tibialTub_angle)
+
+                fr.circle(frame, (point_tibialTub_over.x, point_tibialTub_over.y), color=colors.green)
+                fr.line(frame, (cPatella.x, cPatella.y), (point_tibialTub_over.x, point_tibialTub_over.y))
+                
+                q_angle = self.calc_angle_mid(asis, cPatella, point_tibialTub_over)
+                
+                # Object information
+                fr.put_text(frame, str(int(q_angle)), (cPatella.x + 10, cPatella.y + 50), fontSize=1)
+                
+                fr.meta_info(frame, 'Q angle: ' + str(int(q_angle)))
+                fr.meta_info(frame, 'Condition: ' + self.interpret(q_angle), 'top_left', (0, 50), fontSize=1.5)
+                
+                # Save results
+                self.results.append((int(q_angle), datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')))
+                
+            else:
+                fr.meta_info(frame, 'Keypoints status: invalid', 'bottom_left', fontSize=1.2, color=colors.red)
+            
             
             ret, buffer = cv.imencode('.jpg', frame)
             frame = buffer.tobytes()